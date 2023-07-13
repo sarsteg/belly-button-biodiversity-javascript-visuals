a115e364d3400f71803370c1537f22e92e069b5c
sarsteg <85451611+sarsteg@users.noreply.github.com>
2023-07-13 17:29:52 -0500

    update

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,34 @@
+// URL in constant variable
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
 //Use the D3 library to read in samples.json from URL
-d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(data => {
-    // Extract necessary data from the JSON object
+d3.json(url).then(data => {
+    // Extract data from the JSON object
     const samples = data.samples;
     const metadata = data.metadata;
 
+
 // Create a dropdown menu
 const dropdown = d3.select("#selDataset");
-const options = dropdown.selectAll("option").data(samples).enter().append("option");
-options.text(d => d.id);
+// console.log(dropdown);
+// const options = dropdown.selectAll("option").data(samples).enter().append("option");
+// for (key in samples){
+//     dropdown.append("h6").text(${key.toUpperCase()}':' ${samples[key]});
+//   };
+// options.text(d => d.id);
+
+// console.log(samples);
+// console.log(data);
+
+let sampleNames = data.names;
+    for (let i = 0; i < sampleNames.length; i++){
+      dropdown
+        .append("option")
+        .text(sampleNames[i])
+        .property("value", sampleNames[i]);
+    };
+
+
 
 // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 // Use sample_values as the values for the bar chart.
@@ -24,13 +45,21 @@ function createBarChart(sampleId) {
         y: top10Labels.map(label => `OTU ${label}`),
         text: top10HoverText,
         type: "bar",
-        orientation: "h"
+        orientation: "h", 
+        marker: {
+            color: "rgb(203, 148, 73)"
+        }
     };
 
     const layout = {
-        title: "Top 10 OTUs",
+        title: {
+            text: "<b>Top 10 OTUs</b>",
+            font: {color: "black", size: 20}
+        },
         xaxis: { title: "Sample Values" },
-        yaxis: { title: "OTU IDs" }
+        yaxis: { title: "OTU IDs" },
+        plot_bgcolor: "rgba(0, 0, 0, 0)",
+        paper_bgcolor: "rgba(0, 0, 0, 0)"
     };
 
     Plotly.newPlot("bar", [trace], layout);
@@ -57,9 +86,14 @@ function createBubbleChart(sampleId) {
     };
 
     const layout = {
-        title: "OTU IDs vs Sample Values",
+        title: {
+            text: "<b>OTU IDs vs Sample Values</b>",
+            font: {color: "black", size: 20}
+        },
         xaxis: { title: "OTU IDs" },
-        yaxis: { title: "Sample Values" }
+        yaxis: { title: "Sample Values" },
+        plot_bgcolor: "transparent",
+        paper_bgcolor: "transparent"
     };
 
     Plotly.newPlot("bubble", [trace], layout);
@@ -75,9 +109,55 @@ function displayMetadata(sampleId) {
 
     // Display each key-value pair
     Object.entries(metadataInfo).forEach(([key, value]) => {
-        metadataDiv.append("p").text(`${key}: ${value}`);
+        metadataDiv.append("h6").text(`${key}: ${value}`);
     });
+    // console.log(metadataDiv);
 }
+
+    // Function that builds the gauge chart
+    function createGaugeChart(sampleId) {
+        const sampleMetadata = metadata.find(item => item.id === parseInt(sampleId));
+        const washFrequency = sampleMetadata.wfreq;
+
+        const trace = {
+            value: washFrequency,
+            domain: { x: [0, 1], y: [0, 1] },
+            title: {
+                text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week",
+                font: { color: "black", size: 20 }
+            },
+            type: "indicator",
+            mode: "gauge+number",
+            gauge: {
+                axis: { range: [0, 10], tickmode: "linear", tick0: 2, dtick: 2 },
+                bar: { color: "black" },
+                steps: [
+                    { range: [0, 1], color: "#CB9449" },
+                    { range: [1, 2], color: "#C18D45" },
+                    { range: [2, 3], color: "#B88542" },
+                    { range: [3, 4], color: "#AE7E3E" },
+                    { range: [4, 5], color: "A4773B" },
+                    { range: [5, 6], color: "9B6F37" },
+                    { range: [6, 7], color: "916834" },
+                    { range: [7, 8], color: "886030" },
+                    { range: [8, 9], color: "7E592D" },
+                    { range: [9, 10], color: "745229" }
+                ]
+            }
+        };
+
+        const layout = {
+            width: 400,
+            height: 400,
+            margin: { t: 0, b: 0 },
+            plot_bgcolor: "transparent",
+            paper_bgcolor: "transparent"
+        };
+
+        Plotly.newPlot("gauge", [trace], layout);
+    }
+
+
 
 // Display each key-value pair from the metadata JSON object somewhere on the page.
 
@@ -85,8 +165,19 @@ function displayMetadata(sampleId) {
 function optionChanged(sampleId) {
     createBarChart(sampleId);
     createBubbleChart(sampleId);
+    createGaugeChart(sampleId);
     displayMetadata(sampleId);
 }
+
+// Add the event listener to the dropdown menu
+
+
+dropdown.on("change", function() {
+    let dropdownValue = d3.select("#selDataset").node().value;
+    // console.log(dropdownValue);
+    // const selectedSampleId = d3.event.target.value;
+    optionChanged(dropdownValue);
+});
 
 // Initial rendering
 const initialSampleId = samples[0].id;
--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,95 +1,113 @@
-///////////////////////////////////////////////////////////////////////
-// Advanced Challenge Assignment (Optional)
-// The following task is advanced and therefore optional.
+// Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+    console.log(data);
+  });
+  
+  // Initialize the dashboard at start up 
+  function init() {
+  
+      // Use D3 to select the dropdown menu
+      let dropdownMenu = d3.select("#selDataset");
+  
+      // Use D3 to get sample names and populate the drop-down selector
+      d3.json(url).then((data) => {
+          
+          // Set a variable for the sample names
+          let names = data.names;
+  
+          // Add  samples to dropdown menu
+          names.forEach((id) => {
+  
+              // Log the value of id for each iteration of the loop
+              console.log(id);
+  
+              dropdownMenu.append("option")
+              .text(id)
+              .property("value",id);
+          });
+  
+          // Set the first sample from the list
+          let sample_one = names[0];
+  
+          // Log the value of sample_one
+          console.log(sample_one);
+  
+          // Build the initial plots
+          buildGaugeChart(sample_one);
+      });
+  };
+  
+  // Function that builds the gauge chart
+  function buildGaugeChart(sample) {
+  
+      // Use D3 to retrieve all of the data
+      d3.json(url).then((data) => {
+  
+          // Retrieve all metadata
+          let metadata = data.metadata;
+  
+          // Filter based on the value of the sample
+          let value = metadata.filter(result => result.id == sample);
+  
+          // Log the array of metadata objects after the have been filtered
+          console.log(value)
+  
+          // Get the first index from the array
+          let valueData = value[0];
+  
+          // Use Object.entries to get the key/value pairs and put into the demographics box on the page
+          let washFrequency = Object.values(valueData)[6];
+          
+          // Set up the trace for the gauge chart
+          let trace2 = {
+              value: washFrequency,
+              domain: {x: [0,1], y: [0,1]},
+              title: {
+                  text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week",
+                  font: {color: "black", size: 20}
+              },
+              type: "indicator",
+              mode: "gauge+number",
+              gauge: {
+                  axis: {range: [0,10], tickmode: "linear", tick0: 2, dtick: 2},
+                  bar: {color: "black"},
+                  steps: [
+                      {range: [0, 1], color: "#CB9449"},
+                      {range: [1, 2], color: "#C18D45"},
+                      {range: [2, 3], color: "#B88542"},
+                      {range: [3, 4], color:  "#AE7E3E"},
+                      {range: [4, 5], color:  "A4773B"},
+                      {range: [5, 6], color: "9B6F37"},
+                      {range: [6, 7], color: "916834"},
+                      {range: [7, 8], color:  "886030"},
+                      {range: [8, 9], color: "7E592D"},
+                      {range: [9, 10], color: "745229"},
+                  ]
+              } 
+          };
+  
+          // Set up the Layout
+          let layout = {
+              width: 400, 
+              height: 400,
+              margin: {t: 0, b:0},
+              plot_bgcolor: "transparent",
+              paper_bgcolor: "transparent"
+          };
+  
+          // Call Plotly to plot the gauge chart
+          Plotly.newPlot("gauge", [trace2], layout)
+      });
+  };
 
-// Adapt the Gauge Chart from https://plot.ly/javascript/gauge-charts/ to plot the weekly washing frequency of the individual.
-// Modify the example gauge code to account for values ranging from 0-9
-// Update the chart whenever a new sample is selected
-
-
-// Step 1: Read the JSON data
-d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(data => {
-    // Extract necessary data from the JSON object
-    const samples = data.samples;
-    const metadata = data.metadata;
-
-    // Step 2: Create a dropdown menu to display the top 10 OTUs
-    const dropdown = d3.select("#selDataset");
-    const options = dropdown.selectAll("option").data(samples).enter().append("option");
-    options.text(d => d.id);
-
-    // Step 3: Create a horizontal bar chart
-    function createBarChart(sampleId) {
-        const sampleData = samples.find(sample => sample.id === sampleId);
-        const top10Values = sampleData.sample_values.slice(0, 10).reverse();
-        const top10Labels = sampleData.otu_ids.slice(0, 10).reverse();
-        const top10HoverText = sampleData.otu_labels.slice(0, 10).reverse();
-
-        const trace = {
-            x: top10Values,
-            y: top10Labels.map(label => `OTU ${label}`),
-            text: top10HoverText,
-            type: "bar",
-            orientation: "h"
-        };
-
-        const layout = {
-            title: "Top 10 OTUs",
-            xaxis: { title: "Sample Values" },
-            yaxis: { title: "OTU IDs" }
-        };
-
-        Plotly.newPlot("bar", [trace], layout);
-    }
-
-    // Step 4: Create a bubble chart
-    function createBubbleChart(sampleId) {
-        const sampleData = samples.find(sample => sample.id === sampleId);
-
-        const trace = {
-            x: sampleData.otu_ids,
-            y: sampleData.sample_values,
-            text: sampleData.otu_labels,
-            mode: "markers",
-            marker: {
-                size: sampleData.sample_values,
-                color: sampleData.otu_ids
-            }
-        };
-
-        const layout = {
-            title: "OTU IDs vs Sample Values",
-            xaxis: { title: "OTU IDs" },
-            yaxis: { title: "Sample Values" }
-        };
-
-        Plotly.newPlot("bubble", [trace], layout);
-    }
-
-    // Step 5: Display sample metadata
-    function displayMetadata(sampleId) {
-        const metadataDiv = d3.select("#sample-metadata");
-        const metadataInfo = metadata.find(m => m.id === parseInt(sampleId));
-
-        // Clear previous metadata
-        metadataDiv.html("");
-
-        // Display each key-value pair
-        Object.entries(metadataInfo).forEach(([key, value]) => {
-            metadataDiv.append("p").text(`${key}: ${value}`);
-        });
-    }
-
-    // Step 6: Update all plots when a new sample is selected
-    function optionChanged(sampleId) {
-        createBarChart(sampleId);
-        createBubbleChart(sampleId);
-        displayMetadata(sampleId);
-    }
-
-    // Initial rendering
-    const initialSampleId = samples[0].id;
-    createBarChart(initialSampleId);
-    createBubbleChart(initialSampleId);
-    displayMetadata(initialSampleId);
+  // Event listener for dropdown menu
+d3.select("#selDataset").on("change", function() {
+    // Get selected sample
+    let selectedSample = d3.select("#selDataset").property("value");
+    // Update the gauge chart
+    buildGaugeChart(selectedSample);
 });
+
+  
+  // Call the initialize function
+  init();
